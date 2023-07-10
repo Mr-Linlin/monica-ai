158da4a9adb92ca47ba7d5ee6ca3d989d6399775
‘linzhentao’ <1908878835@qq.com>
2023-07-10 17:15:53 +0800

    #1

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  HttpStatus,
-  Res,
-} from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -26,25 +15,5 @@ export class UsersController {
   async login(@Body() createUserDto: CreateUserDto, @Res() res: any) {
     const data = await this.usersService.login(createUserDto);
     return res.status(HttpStatus.OK).send(data);
-  }
-
-  @Get('list')
-  findAll() {
-    // return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return '';
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
   }
 }
