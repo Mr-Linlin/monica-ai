b4c44b5965936194c9612d68584aa9cd59724613
‘linzhentao’ <1908878835@qq.com>
2024-06-19 17:11:33 +0800

    聊天接口更换

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -11,7 +11,7 @@ import {
 } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
-import { CHAT_URL } from '../config/config';
+import { CHAT_URL_MSG, SESSIONID, APPID, TOKEN, LOGIN_URL_MSG } from '../config/config';
 import { JwtService } from '@nestjs/jwt';
 import { ChatService } from './chat.service';
 import { jwtConstants } from '../auth/constants';
@@ -23,7 +23,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Category } from './entities/category.entity';
 import { Prompt } from './entities/prompt.entity';
-
+import * as fs from 'fs';
+import * as path from 'path';
 interface MessageEvent {
   data: string | object;
 }
@@ -62,33 +63,169 @@ export class ChatController {
           delete item.time;
         });
       }
-      const response = this.httpService.post(CHAT_URL, JSON.stringify(obj), {
-        responseType: 'stream', // 设置响应类型为流
+      let token = TOKEN
+      const msg_content = obj.messages.length > 0 && obj.messages[obj.messages.length - 1].content || '你好'
+      const data = {
+        sessionid: SESSIONID,
+        msg_content
+      }
+      const response = this.httpService.post(CHAT_URL_MSG, JSON.stringify(data), {
         headers: {
           'Content-Type': 'application/json',
+          platform: 'H5',
+          ver: 1.1,
+          ignoreciphertext: 1,
+          appid: APPID,
+          lang: 'en',
+          token,
         },
       });
-
       const chatStream$ = new Observable<string>((observer) => {
         response.subscribe((response) => {
-          response.data.on('data', (chunk: string) => {
-            const message = chunk.toString();
-            console.log(message, '============');
-
-            observer.next(message); // 将从第三方接口返回的数据流实时传递给前端
-            response.data.on('end', () => {
-              observer.complete(); // 在数据流结束时发送 complete 通知
-            });
-          });
-        });
+          // console.log(response.data.data, 'response.data.respons_txt');
+          observer.next(response.data.data.respons_txt);
+          observer.complete();
+          // response.data.data.respons_txt.on('data', (chunk: string) => {
+          //   const message = chunk.toString();
+          //   console.log(message, '============');
+          //   observer.next(message); // 将从第三方接口返回的数据流实时传递给前端
+          //   response.data.data.respons_txt.on('end', () => {
+          //     observer.complete(); // 在数据流结束时发送 complete 通知
+          //   });
+          // });
+        },
+          async (error) => {
+            console.error('Error:', error.response.data);
+            const res = error.response.data
+            if (res.code == 401) {
+              const newToken = await this.refreshToken();
+              const result = await this.makeRequest(newToken)
+              console.log('response', result);
+              if (result?.code == 200) {
+                const res = new Observable<string>((observer) => {
+                  const content = {
+                    content: result.data.respons_txt
+                  }
+                  observer.next(JSON.stringify(content));
+                  observer.complete();
+                });
+                return res.pipe(map((data) => ({ data: { data } })));
+              } else {
+                const res = new Observable<string>((observer) => {
+                  observer.next('{"content":"今日查询次数已达上限"}');
+                  observer.complete();
+                });
+                return res.pipe(map((data) => ({ data: { data } })));
+              }
+            }
+          }
+        );
       });
       return chatStream$.pipe(map((data) => ({ data: { data } })));
+
+      // response.subscribe((response) => {
+      //   console.log(response, 'response', obj);
+
+      //   // response.data.on('data', (chunk: string) => {
+      //   //   const message = chunk.toString();
+      //   //   console.log(message, '============');
+      //   //   observer.next(message); // 将从第三方接口返回的数据流实时传递给前端
+      //   //   response.data.on('end', () => {
+      //   //     observer.complete(); // 在数据流结束时发送 complete 通知
+      //   //   });
+      //   // });
+      // },
+      //   async (error) => {
+      //     console.error('Error:', error.response.data);
+      //     const res = error.response.data
+      //     if (res.code == 401) {
+      //       const newToken = await this.refreshToken();
+      //       const result = await this.makeRequest(newToken)
+      //       console.log('response', result);
+      //       if (result?.code == 200) {
+      //         const res = new Observable<string>((observer) => {
+      //           const content = {
+      //             content: result.data.respons_txt
+      //           }
+      //           observer.next(JSON.stringify(content));
+      //           observer.complete();
+      //         });
+      //         return res.pipe(map((data) => ({ data: { data } })));
+      //       } else {
+      //         const res = new Observable<string>((observer) => {
+      //           observer.next('{"content":"今日查询次数已达上限"}');
+      //           observer.complete();
+      //         });
+      //         return res.pipe(map((data) => ({ data: { data } })));
+      //       }
+      //     }
+      //   });
+      // const res = new Observable<string>((observer) => {
+      //   observer.next('{"content":"今日查询次数已达上限"}');
+      //   observer.complete();
+      // });
+      // return res.pipe(map((data) => ({ data: { data } })));
+      // const chatStream$ = new Observable<string>((observer) => {
+      //   response.subscribe((response) => {
+      //     response.data.on('data', (chunk: string) => {
+      //       const message = chunk.toString();
+      //       console.log(message, '============');
+      //       observer.next(message); // 将从第三方接口返回的数据流实时传递给前端
+      //       response.data.on('end', () => {
+      //         observer.complete(); // 在数据流结束时发送 complete 通知
+      //       });
+      //     });
+      //   });
+      // });
+      // return chatStream$.pipe(map((data) => ({ data: { data } })));
     }
     const res = new Observable<string>((observer) => {
       observer.next('{"content":"今日查询次数已达上限"}');
       observer.complete();
     });
     return res.pipe(map((data) => ({ data: { data } })));
+  }
+  // 刷新token
+  async refreshToken(): Promise<string> {
+    const response = await this.httpService
+      .post(LOGIN_URL_MSG, {}, {
+        headers: {
+          platform: 'H5',
+          ver: 1.1,
+          ignoreciphertext: 1,
+          appid: APPID,
+          lang: 'en',
+        },
+      })
+      .toPromise();
+
+    if (response?.data.code === 200) {
+      const newToken = response.data.data.token;
+      return newToken;
+    } else {
+      throw new Error('登录失败');
+    }
+  }
+  async makeRequest(token: string): Promise<any> {
+    const data = {
+      sessionid: SESSIONID,
+      msg_content: '写一个函数',
+    };
+    const response = await this.httpService
+      .post(CHAT_URL_MSG, JSON.stringify(data), {
+        headers: {
+          'Content-Type': 'application/json',
+          platform: 'H5',
+          ver: 1.1,
+          ignoreciphertext: 1,
+          appid: APPID,
+          lang: 'en',
+          token,
+        },
+      })
+      .toPromise();
+
+    return response?.data;
   }
   @HttpCode(HttpStatus.OK)
   @Post('prompt/category/add')
